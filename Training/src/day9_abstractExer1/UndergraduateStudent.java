2fc29dd6ab5996a6572458e2a6ea6df77ca68cc7
ByrdBassPC <byrdbass@gmail.com>
2022-07-01 15:20:15 -0500

    added logic for abstractExer1

--- Training/src/day9_abstractExer1/UndergraduateStudent.java
+++ Training/src/day9_abstractExer1/UndergraduateStudent.java
@@ -1,16 +1,24 @@
 package day9_abstractExer1;
 
 public class UndergraduateStudent extends Student{
-	private String studentName;
+
+	public UndergraduateStudent(String studentName) {
+		super(studentName);
+	}
 	
-	void generateResult() {
+	public void generateResult() {
+		int sum = 0;
+		for (int score : this.getTestScores()) {
+			sum += score;
+		}
+		float average = sum/this.getTestScores().length;
+		if(average >= 60) {
+			this.setTestResult("Pass");
+		}
+		else {
+			this.setTestResult("Fail");
+		}
 		
 	}
-
-	public UndergraduateStudent(String studentName, String studentName2) {
-		super(studentName);
-		studentName = studentName2;
-	}
-	
 	
 }
